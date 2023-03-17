8d0ed4beb2dd4b7339800cb0d5ff789f0bab4f66
t-takahashi <tkhstol.929@gmail.com>
2023-03-17 09:09:18 +0900

    html作成開始

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,10 @@ const ddbScan = require('../public/javascripts/ddb-scan');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   (async () => {
-    const items = await ddbScan.scanAll();
-    console.debug("--------------------------------------");
-    console.debug(items);
-    console.debug("--------------------------------------");
+    // const items = await ddbScan.scanAll();
 
-    res.render('index', { title: "hoge", data: items });
+    // res.render('index', { title: "hoge", items: JSON.stringify(items) });
+    res.render('index', { title: "hoge", items: 'data=[{"id":"2","name":"idだけのダミーデータ"},{"created_at":"2023-03-12","id":"1","name":"寝室ベッドのシーツ"},{"created_at":"2023-03-12T23:22:39.356Z","id":"0.8894772112652427","name":"hoge"},{"created_at":"2023-03-12T23:22:13.054Z","id":"0.2939987194852651","name":"hoge"}]' });
   })();
 
 });

a824ec975ba55fc42ecfaf686d4b040387c6cddf
t-takahashi <tkhstol.929@gmail.com>
2023-03-19 16:59:29 +0900

    いったんコメントアウト

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ const ddbScan = require('../public/javascripts/ddb-scan');
 router.get('/', function (req, res, next) {
   (async () => {
     const items = await ddbScan.scanAll();
-    res.render('index', { title: "hoge", items: JSON.stringify(items) });
-    // res.render('index', { title: "hoge", items: '[{"id":"2","name":"idだけのダミーデータ"},{"created_at":"2023-03-12","id":"1","name":"寝室ベッドのシーツ"},{"created_at":"2023-03-12T23:22:39.356Z","id":"0.8894772112652427","name":"hoge"},{"created_at":"2023-03-12T23:22:13.054Z","id":"0.2939987194852651","name":"hoge"}]' });
+    // res.render('index', { title: "hoge", items: JSON.stringify(items) });
+    res.render('index', { title: "hoge", items: '[{"id":"2","name":"idだけのダミーデータ"},{"created_at":"2023-03-12","id":"1","name":"寝室ベッドのシーツ"},{"created_at":"2023-03-12T23:22:39.356Z","id":"0.8894772112652427","name":"hoge"},{"created_at":"2023-03-12T23:22:13.054Z","id":"0.2939987194852651","name":"hoge"}]' });
   })();
 });
 
